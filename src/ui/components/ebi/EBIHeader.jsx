57017d313ab22419a1f921ea410334397534895c
Mahdi Mahmoudy <mmahmoudy@ebi.ac.uk>
2018-03-12 16:22:54 +0000

    added a basic sass support with a EBI style overwrites

---
+++ src/ui/components/ebi/EBIHeader.jsx
@@ -0,0 +1,16 @@
+
+import React, { Component } from 'react';
+
+const EBIHeader = () => (
+
+);
+
+EBIHeader.propTypes = {
+  children: PropTypes.node,
+};
+
+EBIHeader.defaultProps = {
+  children: 'Default',
+};
+
+export default EBIHeader;
--- webpack.config.js
+++ webpack.config.js
@@ -16,22 +16,28 @@ module.exports = {
     extensions: [".jsx", ".js"]
   },
   module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.html$/,
-        use: [{
-          loader: 'html-loader',
-          options: { minimize: true }
-        }]
+    rules: [{
+      test: /\.(js|jsx)$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader'
       }
-    ]
+    }, {
+      test: /\.html$/,
+      use: [{
+        loader: 'html-loader',
+        options: { minimize: true }
+      }]
+    }, {
+      test: /\.(css|sass|scss)$/,
+      use: [{
+        loader: "style-loader" // creates style nodes from JS strings
+      }, {
+        loader: "css-loader" // translates CSS into CommonJS
+      }, {
+        loader: "sass-loader" // compiles Sass to CSS
+      }]
+    }]
   },
   plugins: [
     new HtmlWebPackPlugin({
