fd2b4f3a1ff4c8124bedfc3250bec3ac8c264d1d
Mahdi Mahmoudy <mmahmoudy@ebi.ac.uk>
2019-03-25 11:47:54 +0000

    code clean-ups for other/helpers.js

--- src/ui/other/helpers.js
+++ src/ui/other/helpers.js
@@ -1,11 +1,40 @@
 
-export const removeSnakeAndKebabCases = (text) => {
-
-  if ('undefined' === typeof text || null === text) {
-    return;
+// TODO: Replace the use of 'removeSnakeAndKebabCases' with
+// 'removeSnakeCase' and 'removeKebabCase' funcitons.
+// TODO: Provide an aurgument to decide if return valuse
+// should be lower-cased or not.
+const removeSnakeAndKebabCases = (text) => {
+  if (typeof text === 'undefined' || text === null) {
+    return text;
   }
 
   return text
     .replace(/[_-]/g, ' ')
     .toLowerCase();
-}
+};
+
+const removeSnakeCase = (text) => {
+  if (typeof text === 'undefined' || text === null) {
+    return text;
+  }
+
+  return text
+    .replace(/[_]/g, ' ')
+    .toLowerCase();
+};
+
+const removeKebabCase = (text) => {
+  if (typeof text === 'undefined' || text === null) {
+    return text;
+  }
+
+  return text
+    .replace(/[-]/g, ' ')
+    .toLowerCase();
+};
+
+export {
+  removeSnakeAndKebabCases,
+  removeSnakeCase,
+  removeKebabCase,
+};
