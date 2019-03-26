1d36928d929df4ef5a6905bcc83a230464b54324
Mahdi Mahmoudy <mmahmoudy@ebi.ac.uk>
2019-03-26 10:58:20 +0000

    code clean-ups

--- src/ui/components/search/EBIStandardSearch.jsx
+++ src/ui/components/search/EBIStandardSearch.jsx
@@ -5,58 +5,57 @@ import PropTypes from 'prop-types';
 import Button from '../../elements/form/Button';
 
 class EBIStandardSearch extends Component {
-   constructor(props) {
+  constructor(props) {
     super(props);
 
     this.state = {
-      searchTerm: props.term || ''
+      searchTerm: props.term,
     };
   }
 
-  handleInputChange(e) {
+  handleInputChange = (e) => {
     console.log(e.target.value);
     this.setState({
-      searchTerm: e.target.value
+      searchTerm: e.target.value,
     });
   }
 
-  handleSubmit(e) {
-    console.log("submitting...");
+  handleSubmit = (e) => {
+    console.log('submitting...');
     this.props.onSubmit(this.state.searchTerm);
     e.preventDefault();
     e.stopPropagation();
   }
 
   render() {
-
     const { searchTerm } = this.state;
 
-    return(
+    return (
       <div id="ebi-standard-search-field" className="column medium-5">
         <form
           id="ebi_search"
-          onSubmit={this.handleSubmit.bind(this)}
-          >
+          onSubmit={this.handleSubmit}
+        >
           <fieldset>
             <div className="input-group margin-bottom-none margin-top-large">
               <input
                 id="query"
                 className="input-group-field"
                 title="EB-eye Search"
-                tabIndex="1"
+                tabIndex="0"
                 type="text"
                 name="query"
                 size="35"
                 maxLength="2048"
                 placeholder="Search service"
                 value={searchTerm}
-                onChange={this.handleInputChange.bind(this)}
+                onChange={this.handleInputChange}
               />
 
               <div className="input-group-button">
                 <Button
                   type="submit"
-                  onClick={this.handleSubmit.bind(this)}
+                  onClick={this.handleSubmit}
                   className="icon icon-functional"
                 >
                   Search
@@ -65,20 +64,29 @@ class EBIStandardSearch extends Component {
             </div>
           </fieldset>
           <p id="example" className="small">
-            Examples: <a href="/ebisearch/search.ebi?db=allebi&amp;requestFrom=ebi_index&amp;query=blast">blast</a>, <a href="/ebisearch/search.ebi?db=allebi&amp;query=keratin&amp;requestFrom=ebi_index">keratin</a>, <a href="/ebisearch/search.ebi?db=allebi&amp;query=bfl1&amp;requestFrom=ebi_index">bfl1</a>
-            <a className="float-right" href="#"><span className="icon icon-generic" data-icon="("></span> advanced search</a>
+            <span>Examples:&nbsp;</span>
+            {/* eslint-disable-next-line */}
+            <a href="/ebisearch/search.ebi?db=allebi&amp;requestFrom=ebi_index&amp;query=blast">blast</a>,
+            {/* eslint-disable-next-line */}
+            <a href="/ebisearch/search.ebi?db=allebi&amp;query=keratin&amp;requestFrom=ebi_index">keratin</a>,
+            {/* eslint-disable-next-line */}
+            <a href="/ebisearch/search.ebi?db=allebi&amp;query=bfl1&amp;requestFrom=ebi_index">bfl1</a>
+            {/* eslint-disable-next-line */}
+            <button className="float-right"><span className="icon icon-generic" data-icon="(" /> advanced search</button>
           </p>
         </form>
       </div>
-    )
+    );
   }
 }
 
 EBIStandardSearch.propTypes = {
+  term: PropTypes.string,
   onSubmit: PropTypes.func,
 };
 
 EBIStandardSearch.defaultProps = {
+  term: '',
   onSubmit: () => undefined,
 };
 
