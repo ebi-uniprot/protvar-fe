2d3228f1c205c31fcd0b0436d08dd60b44dc254c
Mahdi Mahmoudy <mmahmoudy@ebi.ac.uk>
2019-04-16 14:29:02 +0100

    basic Structural Significances

--- src/ui/components/significances/ExpandedStructuralSignificance.jsx
+++ src/ui/components/significances/ExpandedStructuralSignificance.jsx
@@ -1,32 +1,114 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const ExpandedStructuralSignificance = (props) => {
-  const { data } = props;
-  return (
-    <tr>
-      <td colSpan="11">
-        <span className="expanded-section-title">Structural Significances</span>
-        {props.detailsLink}
+class ExpandedStructuralSignificance extends Component {
 
-        <ul className="column-list">
-          {data.map(s => (
-            <li>
-              <a href={`https://www.ebi.ac.uk/pdbe/entry/pdb/${s.id}`} target="_blank">{s.id}</a>
-            </li>
-          ))}
-        </ul>
-      </td>
-    </tr>
-  );
+  state = {
+    structure: null,
+  };
+
+  structureChange = ({ target }) => {
+    this.setState({
+      structure: target.value,
+    });
+  };
+
+  render() {
+    const { structure } = this.state;
+    const { data, detailsLink } = this.props;
+    let imageUrl;
+
+    const bestStructures = data.structures
+      .reduce((all, current) => {
+        return all.concat(current.best_structures);
+      },[]);
+
+    const ligands = data.ligands
+      .reduce((all, current) => {
+        return all.concat(current.ligands);
+      },[]);
+
+    const interactions = data.interactions
+      .reduce((all, current) => {
+        return all.concat(current.partners);
+      },[]);
+
+    if (!structure && bestStructures.length > 0) {
+      imageUrl = `https://www.ebi.ac.uk/pdbe/static/entry/${bestStructures[0]}_single_entity_1_image-200x200.png`;
+    } else if (structure) {
+      imageUrl = `https://www.ebi.ac.uk/pdbe/static/entry/${structure}_single_entity_1_image-200x200.png`;
+    }
+
+    return (
+      <tr>
+        <td colSpan="11">
+          <span className="expanded-section-title">Structural Significances</span>
+          {detailsLink}
+
+          <div className="significances-groups">
+            <div className="column">
+              <div><b>2D Image</b></div>
+              {(imageUrl) && <img src={imageUrl} />}
+            </div>
+
+            <div className="column">
+              <b>Best Structures ({bestStructures.length})</b>
+              {(bestStructures.length > 0) && <select multiple size="5" onChange={e => this.structureChange(e)}>
+                {bestStructures.map((s) => {
+                    return <option value={s}>{s}</option>
+                  })
+                };
+              </select>}
+            </div>
+
+            <div className="column">
+              <b>Ligands ({ligands.length})</b>
+              {(ligands.length > 0) && <ul data-columns="2">
+                {ligands.map(s => <li>{s}</li>)}
+              </ul>}
+            </div>
+
+            <div className="column">
+              <b>Interactions ({interactions.length})</b>
+              {(interactions.length > 0) && <ul data-columns="2">
+                {interactions.map(s => <li>{s}</li>)}
+              </ul>}
+            </div>
+          </div>
+        </td>
+      </tr>
+    );
+  }
 };
 
 ExpandedStructuralSignificance.propTypes = {
-  data: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.string,
-    method: PropTypes.string,
-    range: PropTypes.arrayOf(PropTypes.number),
-  })),
+  data: PropTypes.shape({
+    allStructures: PropTypes.arrayOf(PropTypes.string),
+    annotations: PropTypes.arrayOf(PropTypes.shape({
+      count_data_resource: PropTypes.number,
+      data_resource: PropTypes.arrayOf(PropTypes.string),
+      position: PropTypes.number,
+      position_code: PropTypes.string,
+    })),
+    interactions: PropTypes.arrayOf(PropTypes.shape({
+      count_partners: PropTypes.number,
+      partners: PropTypes.arrayOf(PropTypes.string),
+      position: PropTypes.number,
+      position_code: PropTypes.string,
+    })),
+    ligands: PropTypes.arrayOf(PropTypes.shape({
+      count_ligands: PropTypes.number,
+      ligands: PropTypes.arrayOf(PropTypes.string),
+      position: PropTypes.number,
+      position_code: PropTypes.string,
+    })),
+    structures: PropTypes.arrayOf(PropTypes.shape({
+      count_best_structures: PropTypes.number,
+      best_structures: PropTypes.arrayOf(PropTypes.string),
+      position: PropTypes.number,
+      position_code: PropTypes.string,
+    })),
+  }),
   detailsLink: PropTypes.element.isRequired,
 };
 
