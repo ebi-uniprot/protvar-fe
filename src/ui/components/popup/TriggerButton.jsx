04905a7b824ddd13a12f497bf539ef228e6f8a86
Rizwan Ishtiaq <rizwan.ishtiaq@hotmai.com>
2021-09-13 14:11:51 +0100

    deletes unused files

--- src/ui/components/popup/TriggerButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-const Trigger = ({ triggerText, buttonRef, showModal }) => {
-	return (
-		<button className="btn btn-lg btn-danger center modal-button" ref={buttonRef} onClick={showModal}>
-			{triggerText}
-		</button>
-	);
-};
-export default Trigger;
