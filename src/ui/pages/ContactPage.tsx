cd63f5d4e6d79479bd59b9d515c4c5dc59c8ae01
Rizwan Ishtiaq <rizwan.ishtiaq@hotmail.com>
2021-09-28 14:58:52 +0100

    forms taking time to load

--- src/ui/pages/ContactPage.tsx
+++ src/ui/pages/ContactPage.tsx
@@ -1,13 +1,18 @@
-import Loader from "../elements/Loader";
 import DefaultPageLayout from "../layout/DefaultPageLayout";
 
 function ContactPageContent() {
   return <>
-    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfVhyTjeH5NG0QnYU0LozFR1tlkiNvl8nN5g5pIrcL76yO4YA/viewform?embedded=true"
-      width="760" height="970" frameBorder={0} marginHeight={0} marginWidth={0} title="Contact us"
-    >
-      <Loader />
-    </iframe>
+    <div className="container">
+      <h3>Contact us</h3>
+      <p>Use below form to contact us. You can also email us directly on Ap1e2Bp3Cv4De5p@6e7b8i9.ac.uk,
+        especially if you would like to send attachments or screenshots.
+        Remove all the number and capital letters from email address.</p>
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfVhyTjeH5NG0QnYU0LozFR1tlkiNvl8nN5g5pIrcL76yO4YA/viewform?embedded=true"
+        width="760" height="970" frameBorder={0} marginHeight={0} marginWidth={0} title="Contact us"
+      >
+        Loading...
+      </iframe>
+    </div>
   </>
 }
 
