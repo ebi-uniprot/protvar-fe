b9b2c41ee48f0fb05a19d03a67e0801fd2a67f31
Rizwan Ishtiaq <rizwan.ishtiaq@hotmai.com>
2021-11-30 14:53:37 +0000

    space management for better readability

--- src/ui/components/search/CaddLegendColors.tsx
+++ src/ui/components/search/CaddLegendColors.tsx
@@ -1,24 +1,34 @@
+import { EmptyElement } from "../../../constants/Const";
 import { getColor, getTitle } from "./CaddHelper";
 
 function CaddLegendColors() {
   return (
     <div className="search-results-legends" style={{ float: "unset" }}>
-      <strong>CADD colour indications</strong>
+      <strong>CADD phred-like score colour</strong>
       <br />
-      <ul>
+      <br />
+      <div className="flex-column">
         <CaddColourLi cadd={1} />
         <CaddColourLi cadd={15} />
         <CaddColourLi cadd={20} />
         <CaddColourLi cadd={25} />
         <CaddColourLi cadd={30} />
-      </ul>
+      </div>
     </div>
   );
 }
 
 function CaddColourLi(props: { cadd: number }) {
-  return <li>
-    <span className="legend-icon button--legends" style={{ backgroundColor: getColor(props.cadd) }} /> {getTitle(props.cadd.toString())}
-  </li>
+  const texts = getTitle(props.cadd.toString())?.split("-")
+  if (!texts || texts.length < 3)
+    return EmptyElement
+  return <>
+    <div className="flex">
+      <ul><li><span className="legend-icon button--legends" style={{ backgroundColor: getColor(props.cadd) }} /></li></ul>
+      <div className="flex1">{texts[0]}</div>
+      <div className="flex2">{texts[1]}</div>
+      <div className="flex6">{texts[2]}</div>
+    </div>
+  </>
 }
 export default CaddLegendColors;
