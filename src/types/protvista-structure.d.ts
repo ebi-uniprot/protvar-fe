3996cd3c69caf222e82e9212c8c93fe58a1931ef
prabhat <prabhat@ebi.ac.uk>
2023-01-26 14:50:47 +0000

    removed protvista struct and updated package json

--- src/types/protvista-structure.d.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-declare module 'protvista-structure';
-
-declare namespace JSX {
-  interface IntrinsicElements {
-    'protvista-structure': any;
-  }
-}
