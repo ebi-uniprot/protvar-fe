63d89cffc01f861197acf3c1b12349e5d267e5e1
prabhat <prabhat@ebi.ac.uk>
2023-01-26 14:45:18 +0000

    refactored common utils func

--- src/ui/components/function/ResidueRegionTable.tsx
+++ src/ui/components/function/ResidueRegionTable.tsx
@@ -7,7 +7,7 @@ import {Pocket, Foldx, P2PInteraction, ProteinFeature, FunctionalResponse} from 
 import { ReactComponent as ChevronDownIcon } from "../../../images/chevron-down.svg"
 import { v1 as uuidv1 } from 'uuid';
 import { StringVoidFun } from "../../../constants/CommonTypes";
-import { getKeyValue } from "../../../utills/Util";
+import {formatRange, getKeyValue} from "../../../utills/Util";
 
 interface ResidueRegionTableProps {
   apiData: FunctionalResponse
@@ -253,65 +253,9 @@ function getList(list: Array<JSX.Element>, rowKey: string, expendedRowKey: strin
     </ul>
   }
 }
-
+/*
 const NoData = () => {
   return <label style={{ marginLeft: '10px' }}>No data</label>
-}
-
-function formatRange(xs: number[]) {
-  if (xs.length === 0)
-    return ''
-  if (xs.length === 1)
-    return xs[0].toString()
-  xs.sort(function (a, b) {
-    return a - b
-  });
-  let start = null;
-  let end = null;
-  let str = ''
-
-  for (let i = 0; i < xs.length; i++) {
-    var num = xs[i];
-    //initialize
-    if (start == null || end == null) {
-      start = num;
-      end = num;
-    }
-    //next number in range
-    else if (end === num - 1) {
-      end = num;
-    }
-    //there's a gap
-    else {
-      //range length 1
-      if (start === end) {
-        str += start + ",";
-      }
-      //range length 2
-      else if (start === end - 1) {
-        str += start + "," + end + ",";
-      }
-      //range lenth 2+
-      else {
-        str += start + "-" + end + ",";
-      }
-      start = num;
-      end = num;
-    }
-  }
-  if (start !== null && end !== null) {
-    if (start === end) {
-      str += start;
-    } else if (start === end - 1) {
-      str += start + "," + end;
-    } else {
-      str += start + "-" + end;
-    }
-  }
-  if (str.endsWith(",")) {
-    str = str.substring(0, str.length - 1)
-  }
-  return str
-}
+}*/
 
 export default ResidueRegionTable;
--- src/utills/Util.ts
+++ src/utills/Util.ts
@@ -7,3 +7,60 @@ export const fullAminoAcidName = (key: string | undefined | null) => {
     return ""
   return AMINO_ACID_FULL_NAME.get(key.toLowerCase())
 }
+
+
+export function formatRange(xs: number[]) {
+  if (xs.length === 0)
+    return ''
+  if (xs.length === 1)
+    return xs[0].toString()
+  xs.sort(function (a, b) {
+    return a - b
+  });
+  let start = null;
+  let end = null;
+  let str = ''
+
+  for (let i = 0; i < xs.length; i++) {
+    var num = xs[i];
+    //initialize
+    if (start == null || end == null) {
+      start = num;
+      end = num;
+    }
+    //next number in range
+    else if (end === num - 1) {
+      end = num;
+    }
+    //there's a gap
+    else {
+      //range length 1
+      if (start === end) {
+        str += start + ",";
+      }
+      //range length 2
+      else if (start === end - 1) {
+        str += start + "," + end + ",";
+      }
+      //range lenth 2+
+      else {
+        str += start + "-" + end + ",";
+      }
+      start = num;
+      end = num;
+    }
+  }
+  if (start !== null && end !== null) {
+    if (start === end) {
+      str += start;
+    } else if (start === end - 1) {
+      str += start + "," + end;
+    } else {
+      str += start + "-" + end;
+    }
+  }
+  if (str.endsWith(",")) {
+    str = str.substring(0, str.length - 1)
+  }
+  return str
+}
