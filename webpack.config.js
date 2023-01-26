d38c3d89f737d2a480dd60fcc6052f53eb7bfd84
prabhat <prabhat@ebi.ac.uk>
2023-01-26 18:02:04 +0000

    enable webpack compression

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const CompressionPlugin = require("compression-webpack-plugin");
+
+module.exports = {
+    plugins: [new CompressionPlugin(),
+        new CompressionPlugin({
+            filename: "[path][base].br",
+            algorithm: "brotliCompress",
+            test: /\.(js|css|html|svg)$/,
+            compressionOptions: {
+                params: {
+                    [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+                },
+            },
+            threshold: 10240,
+            minRatio: 0.8,
+            deleteOriginalAssets: false,
+        })],
+};
