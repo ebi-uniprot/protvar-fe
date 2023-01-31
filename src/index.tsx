8299a00a8446dcb0fe91c7af9bca519f1e6a7853
prabhat <prabhat@ebi.ac.uk>
2023-01-31 13:52:34 +0000

    revert react and other pkg upgrade

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import {createRoot} from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './styles/index.scss';
 import reportWebVitals from './reportWebVitals';
 
 import App from './ui/App';
 
-const container = document.getElementById('root');
-if (!container) throw new Error('Failed to find the root element');
-const root = createRoot(container);
-root.render(<BrowserRouter basename={process.env.PUBLIC_URL}>
-	<App />
-</BrowserRouter>);
+ReactDOM.render(
+	<BrowserRouter basename={process.env.PUBLIC_URL}>
+		<App />
+	</BrowserRouter>,
+	document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
