351a42e187d45e8c5a5fb27d22d25ead197ff99b
prabhat <prabhat@ebi.ac.uk>
2023-02-08 12:30:05 +0000

    contact page changes

--- src/constants/const.ts
+++ src/constants/const.ts
@@ -3,3 +3,5 @@ export const NO_OF_ITEMS_PER_PAGE = 25;
 export const MAX_IN_PLACE_DOWNLOAD_WITHOUT_EMAIL = 25;
 export const G2P_MAPPING_URI = `${API_URL}/mappings`;
 export const API_HEADERS = { "Content-Type": "application/json", Accept: "*" };
+export const USER = 'protvar';
+export const DOMAIN = 'ebi.ac.uk';
