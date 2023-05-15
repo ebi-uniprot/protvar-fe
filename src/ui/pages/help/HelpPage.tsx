f195b13bd6960bd6a4988d5934ada0b26e386439
prabhat <prabhat@ebi.ac.uk>
2023-05-15 16:11:56 +0100

    fixed hash link

--- src/ui/pages/help/HelpPage.tsx
+++ src/ui/pages/help/HelpPage.tsx
@@ -2,20 +2,47 @@ import DefaultPageLayout from "../../layout/DefaultPageLayout";
 import "./HelpPage.css";
 import React, {useEffect, useState} from "react";
 import ReactMarkdown from "react-markdown";
-//import remarkGfm from 'remark-gfm';
+import remarkGfm from 'remark-gfm';
 import rehypeRaw from "rehype-raw";
+import {uriTransformer} from "react-markdown";
+import {useLocation } from 'react-router-dom';
 
 function HelpPageContent() {
     const [markdown, setMarkdown] = useState("")
+
+    const addHelpPrefix = function (uri: string) {
+        if (uri.startsWith('#'))
+            return window.location.pathname + uri
+        return uriTransformer(uri)
+    }
 
     useEffect(() => {
         fetch(window.location.href + ".md")
             .then((res) => res.text())
             .then((text) => setMarkdown(text));
-    });
+    }, []);
+
+
+    const { hash } = useLocation();
+    useEffect(() => {
+        // if not a hash link, scroll to top
+        if (hash === '') {
+            window.scrollTo(0, 0);
+        }
+        // else scroll to id
+        else {
+            setTimeout(() => {
+                const id = hash.replace('#', '');
+                const element = document.getElementById(id);
+                if (element) {
+                    element.scrollIntoView();
+                }
+            }, 0);
+        }
+    }, [hash]); // do this on route change
 
     return <div className="container help">
-        <ReactMarkdown rehypePlugins={[rehypeRaw]}>{markdown}</ReactMarkdown>
+        <ReactMarkdown rehypePlugins={[rehypeRaw]} transformLinkUri={addHelpPrefix} remarkPlugins={[remarkGfm]}>{markdown}</ReactMarkdown>
     </div>
 }
 
