957de661fe020ac91c160d49e382ad92e75b05f6
prabhat <prabhat@ebi.ac.uk>
2023-05-22 15:46:38 +0100

    removed xrefs display condition

--- src/ui/components/population/common/XReferenceLi.tsx
+++ src/ui/components/population/common/XReferenceLi.tsx
@@ -3,9 +3,9 @@ interface XReferenceLiProps {
   url: string
 }
 function XReferenceLi(props: XReferenceLiProps) {
-  if (props.id && (props.id.startsWith('COS') || props.id.startsWith('RCV') || props.id.startsWith('rs')))
+  //if (props.id && (props.id.startsWith('COS') || props.id.startsWith('RCV') || props.id.startsWith('rs')))
     return <li><a href={props.url} target="_blank" rel="noreferrer">{props.id}</a></li>
-  else
-    return <></>;
+  //else
+  //  return <></>;
 }
 export default XReferenceLi;
