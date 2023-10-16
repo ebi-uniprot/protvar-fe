0423d9a3fdfd158ae2ccbb387a602c71b22f0705
prabhat <prabhat@ebi.ac.uk>
2023-10-16 16:28:29 +0100

    color constants

---
+++ src/types/Colors.ts
@@ -0,0 +1,8 @@
+export const BLACK = {r:0, g:0, b:0}
+export const BLUE = {r:0, g:0 , b: 255}
+export const GREEN = {r:0, g:255 , b: 0}
+export const CYAN = {r:0, g:255, b:255}
+export const RED = {r:255, g:0 , b: 0}
+export const MAGENTA = {r:255, g:0, b:255}
+export const YELLOW = {r: 255, g: 255, b: 0}
+export const WHITE = {r:255, g:255, b:255}
