22c8df56c21dcbb88741bf3b4880f064553228f0
prabhat <prabhat@ebi.ac.uk>
2023-10-16 16:38:45 +0100

    simplified table, added interaction buttons, fixed select/highlight resids in correct chain

--- src/ui/components/structure/InteractionInfoTable.tsx
+++ src/ui/components/structure/InteractionInfoTable.tsx
@@ -21,28 +21,27 @@ interface InteractionInfoTableProps {
   interactionData: Array<P2PInteraction>,
   selectedInteraction: string,
   setSelected: any
+  aaPos: number
   pdbeRef: PdbeRef
 }
 
 function InteractionInfoTable(props: InteractionInfoTableProps) {
 
-  return <>
-    <table>
+  return <div className="tableFixHead">
+      <table>
         <thead>
           <tr>
-            <th colSpan={5}>Predicted Interacting Structure <a href="https://pubmed.ncbi.nlm.nih.gov/36690744" target="_blank" rel="noreferrer"><ExternalLinkIcon width={12.5}/></a></th>
+            <th colSpan={3}>Predicted Interacting Structure <a href="https://pubmed.ncbi.nlm.nih.gov/36690744" target="_blank" rel="noreferrer"><ExternalLinkIcon width={12.5}/></a></th>
           </tr>
           <tr>
-              <th>A</th>
-              <th>Residues</th>
-              <th>B</th>
-              <th>Residues</th>
+              <th>Chain A</th>
+              <th>Chain B</th>
               <th>pDockQ</th>
           </tr>
         </thead>
-        <tbody>{getInteractionInfoRows(props)}</tbody>
+        {getInteractionInfoRows(props)}
       </table>
-  </>
+  </div>
 }
 
 function getInteractionInfoRows(props: InteractionInfoTableProps) {
@@ -53,34 +52,46 @@ function getInteractionInfoRows(props: InteractionInfoTableProps) {
   return rows;
 }
 
-
 function getInteractionInfoRow(i: P2PInteraction, props: InteractionInfoTableProps) {
-  const isRowSelected = props.selectedInteraction === (i.a+"_"+i.b)
+  const rowId = i.a+"_"+i.b
+  const isRowSelected = props.selectedInteraction === rowId
   const rowClass = isRowSelected ? 'clickable-row active' : 'clickable-row';
+  const protChain = i.a === props.isoFormAccession ? 'A' : 'B';
 
-  const ia = isRowSelected ? <u onMouseOut={(_) => props.pdbeRef.clearSelect()} onMouseOver={(_) => props.pdbeRef.selectChain('A')}>{i.a}</u> : <>{i.a}</>
-  const ib = isRowSelected ? <u onMouseOut={(_) => props.pdbeRef.clearSelect()} onMouseOver={(_) => props.pdbeRef.selectChain('B')}>{i.b}</u> : <>{i.b}</>
-  const formattedAResids = formatRange(i.aresidues);
-  const formattedBResids = formatRange(i.bresidues);
-  const trimmedAResids = formattedAResids.substring(0, 12) + '...';
-  const trimmedBResids = formattedBResids.substring(0, 12) + '...';
-  const aResids = isRowSelected ? <u onMouseOver={(_) => props.pdbeRef.highlightResids(i.aresidues, 'A')}>{trimmedAResids}</u> : <>{trimmedAResids}</>
-  const bResids = isRowSelected ? <u onMouseOver={(_) => props.pdbeRef.highlightResids(i.bresidues, 'B')}>{trimmedBResids}</u> : <>{trimmedBResids}</>
+  const aResids = 'Residues: ' + formatRange(i.aresidues);
+  const bResids = 'Residues: ' + formatRange(i.bresidues);
   const modelUrl = API_URL + '/interaction/'+i.a+'/'+i.b+'/model';
 
   const clicked = () => {
-    props.pdbeRef.update(customSettings(modelUrl));
-    props.setSelected(i)
+      props.setSelected(i)
+      props.pdbeRef.update(customSettings(modelUrl)).then(() =>
+        props.pdbeRef.subscribeOnload(props.aaPos, protChain)
+      );
   }
 
-  return (
-    <tr className={rowClass} onClick={clicked} key={(i.a+"_"+i.b)}>
-        <td className="small">{ia}</td>
-        <td className="small" title={formattedAResids}>{aResids}</td>
-        <td className="small">{ib}</td>
-        <td className="small" title={formattedBResids}>{bResids}</td>
-        <td className="small">{i.pdockq.toFixed(3)}</td>
+  const highlightInterface = () => {
+      props.pdbeRef.highlightInterface(i.aresidues, i.bresidues, props.aaPos, protChain)
+  }
+
+  const options = isRowSelected ?
+    <tr className="active" key={'options-'+rowId}>
+        <td className="small" colSpan={3}>
+            <button className="button-new" onClick={() => props.pdbeRef.zoomToVariant(props.aaPos, protChain)}>Zoom to variant</button>
+            <button className="button-new" onClick={highlightInterface}>Highlight Interface</button>
+            <button className="button-new" onClick={() => props.pdbeRef.resetDefault(props.aaPos, protChain)}>Reset</button>
+        </td>
     </tr>
+    : <></>
+
+  return (
+      <tbody key={rowId}>
+        <tr className={rowClass} onClick={clicked} key={(i.a+"_"+i.b)}>
+            <td className="small" title={aResids}>{i.a}</td>
+            <td className="small" title={bResids}>{i.b}</td>
+            <td className="small">{i.pdockq.toFixed(3)}</td>
+        </tr>
+        {options}
+      </tbody>
   );
 }
 export default InteractionInfoTable;
