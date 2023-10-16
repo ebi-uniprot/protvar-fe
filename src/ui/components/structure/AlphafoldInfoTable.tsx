e3431fccf9432fb27302007b1378da7f97324e15
prabhat <prabhat@ebi.ac.uk>
2023-10-16 16:37:33 +0100

    added AF interaction buttons

--- src/ui/components/structure/AlphafoldInfoTable.tsx
+++ src/ui/components/structure/AlphafoldInfoTable.tsx
@@ -1,9 +1,10 @@
-import { ALPHAFOLD_URL_INTERFACE_BY_PROTEIN } from '../../../constants/ExternalUrls';
+import {ALPHAFOLD_URL_INTERFACE_BY_PROTEIN} from '../../../constants/ExternalUrls';
 import {formatRange} from "../../../utills/Util";
 import PdbeRef from "./PdbeRef";
 import {AlphafoldResponseElement } from "../../../types/AlphafoldResponse";
 import {baseSettings} from "./StructuralDetail";
 import {Pocket} from "../../../types/FunctionalResponse";
+import {ReactComponent as ExternalLinkIcon} from "../../../images/external-link.svg";
 
 const afSettings = (alphaFoldUrl: string) => {
   return {...baseSettings,
@@ -29,114 +30,117 @@ function AlphafoldInfoTable(props: AlphafoldInfoTableProps) {
   const alphaFoldUrl = props.alphaFoldData[0].cifUrl
   const isRowSelected = props.selectedAlphaFoldId === alphaFoldId;
   const rowClass = isRowSelected ? 'clickable-row active' : 'clickable-row';
-  const paeImg = props.alphaFoldData[0].paeImageUrl;
 
-  const id = isRowSelected ? <u onMouseOver={(_) => props.pdbeRef.clearSelect()}>{alphaFoldId}</u> : <>{alphaFoldId}</>
-  const pos = isRowSelected ? <u onMouseOver={(_) => props.pdbeRef.selectPos(props.aaPos)}>{props.aaPos}</u> : <>{props.aaPos}</>
+  let pocketsList: Array<JSX.Element> = [];
+  let pocketsBtn: Array<JSX.Element> = [];
+  let pnum = 0;
+
+  props.pocketData.forEach((pocket) => {
+    pnum++
+    const p = 'P' + pnum
+    const formattedPockets = 'Residues: ' + formatRange(pocket.residList)
+    const highlightText = pnum === 1 ? 'Highlight ' + p : p
+    pocketsList.push(<span key={'pocketsList-'+pnum} title={formattedPockets}>{p}</span>);
+    pocketsBtn.push(<button key={'pocketsBtn-'+pnum} title={formattedPockets} className="button-new" onClick={() => props.pdbeRef.highlightPocket(props.aaPos, pocket.residList)}>{highlightText}</button>)
+  });
+
   const clicked = () => {
-    props.pdbeRef.update(afSettings(alphaFoldUrl));
     props.setSelected(props.alphaFoldData[0])
+    props.pdbeRef.update(afSettings(alphaFoldUrl)).then(() =>
+        props.pdbeRef.subscribeOnload(props.aaPos)
+    );
   }
 
-  return (
-    <div>
-      <table>
-        <tbody>
+  const options = isRowSelected ?
+      <tr className="active" key={'options-'+alphaFoldId}>
+        <td className="small" colSpan={3}>
+          <button className="button-new" onClick={() => props.pdbeRef.zoomToVariant(props.aaPos)}>Zoom to variant</button>
+          {pocketsBtn}
+          <button className="button-new" onClick={() => props.pdbeRef.resetDefault(props.aaPos)}>Reset</button>
+        </td>
+      </tr>
+      : <></>
+
+  return <div>
+    <table>
+          <thead>
           <tr>
-            <th colSpan={4}>Predicted Structure</th>
+            <th colSpan={3}>AlphaFold Predicted Structure <a href={ALPHAFOLD_URL_INTERFACE_BY_PROTEIN + props.isoFormAccession} target="_blank" rel="noreferrer" title="Click for further information from AlphaFold"><ExternalLinkIcon width={12.5}/></a></th>
           </tr>
           <tr>
-            <th>Source</th>
-            <th>Identifier</th>
+            <th>ID</th>
             <th>Position</th>
             <th>Pockets</th>
           </tr>
+          </thead>
+        <tbody key={alphaFoldId}>
           <tr className={rowClass} onClick={clicked}>
-            <td className="small">
-              <a href={ALPHAFOLD_URL_INTERFACE_BY_PROTEIN + props.isoFormAccession} target="_blank" rel="noreferrer">
-                <u>AlphaFold</u>
-              </a>
-            </td>
-            <td className="small">{id}</td>
-            <td className="small">{pos}</td>
-            <td className="small"><Pockets {...props} /></td>
+            <td className="small">{alphaFoldId}</td>
+            <td className="small">{props.aaPos}</td>
+            <td className="small"><>{props.pocketData.length === 0 ? '-' : pocketsList}</></td>
           </tr>
+          {options}
         </tbody>
       </table>
-      {isRowSelected && <ModelConfidenceAndPAE paeImg={paeImg} />}
-    </div>
-  );
+    {isRowSelected && <ModelConfidence />}
+  </div>
 }
 
-
-const Pockets = (props: AlphafoldInfoTableProps) => {
-  if (props.pocketData.length === 0) return <>N/A</>;
-
-  let pocketsList: Array<JSX.Element> = [];
-  let counter = 0;
-  const isRowSelected = props.selectedAlphaFoldId === props.alphaFoldData[0].entryId;
-
-  props.pocketData.forEach((pocket) => {
-    counter = counter + 1;
-    const formattedPockets = formatRange(pocket.residList)
-    const trimmedPockets = formattedPockets.substring(0, 12) + '...'
-    const p = isRowSelected ? <u title={formattedPockets} onMouseOver={(e) => props.pdbeRef.selectPocket(pocket.residList)}>{trimmedPockets}</u> : <span title={formattedPockets}>{trimmedPockets}</span>
-    pocketsList.push(<span key={'pocket-'+counter}>{p}</span>);
-  });
-
-  return <>{pocketsList}</>
+export function ModelConfidence() {
+    return (
+        <div className="search-results-legends" style={{ float: "unset" }}>
+            <strong>Model Confidence</strong>
+            <br/>
+            <br/>
+            <div className="flex-column">
+                <div className="flex">
+                    <div className="legend-icon button--legends button--legends--high"></div>
+                    <div className="flex1">Very high (pLDDT &gt; 90)</div>
+                </div>
+                <div className="flex">
+                    <div className="legend-icon button--legends button--legends--confident"></div>
+                    <div className="flex1">Confident (90 &gt; pLDDT &gt; 70)</div>
+                </div>
+                <div className="flex">
+                    <div className="legend-icon button--legends button--legends--low"></div>
+                    <div className="flex1">Low (70 &gt; pLDDT &gt; 50)</div>
+                </div>
+                <div className="flex">
+                    <div className="legend-icon button--legends button--legends--verylow"></div>
+                    <div className="flex1">Very low (pLDDT &lt; 50)</div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-
-function ModelConfidenceAndPAE(props: { paeImg: any; }) {
-  return (
-      <div className="search-results-legends small">
-        <div>
-          <strong>Model Confidence</strong>
-          <br />
-          <ul>
-            <li>
-              <span className="legend-icon button--legends button--legends--high" /> Very high (pLDDT &gt; 90)
-            </li>
-            <li>
-              <span className="legend-icon button--legends button--legends--confident" /> Confident (90 &gt; pLDDT &gt; 70)
-            </li>
-            <li>
-              <span className="legend-icon button--legends button--legends--low" /> Low (70 &gt; pLDDT &gt; 50)
-            </li>
-            <li>
-              <span className="legend-icon button--legends button--legends--verylow" /> Very low (pLDDT &lt; 50)
-            </li>
-          </ul>
-        </div>
-        <div>
-          <strong>Predicted Align Error</strong>
-          <br />
-          <div>
-            <div className="pae-axis pae-y-axis">
-              Aligned residue
+export function PAE(props: { paeImg: any; }) {
+    return (
+        <div style={{marginTop: '10px'}}>
+            <strong>Predicted Align Error</strong>
+            <br/>
+            <br/>
+            <div className="pae">
+                <span className="pae-axis pae-y-axis">
+                    Aligned residue
+                </span>
+                <img width="150px" height="150px" src={props.paeImg} alt="AlphaFold PAE"/>
+                <div style={{paddingLeft: '15px'}}>
+                    AlphaFold produces a per-residue confidence score (pLDDT) between 0 and 100. Some regions with
+                    low pLDDT may be unstructured in isolation.
+                    <br/>
+                    The colour at position (x, y) indicates AlphaFold's expected position error at residue x, when
+                    the predicted and true structures are aligned on residue y.
+                    <br/>
+                    This is useful for assessing inter-domain accuracy.
+                </div>
+                <div></div>
+                <div className="pae-axis pae-x-axis">
+                    Scored residue
+                </div>
             </div>
-            <div className="pae-img">
-              <img src={props.paeImg} alt="AlphaFold PAE" className={"pae-img-box-border "}/>
-            </div>
-          </div>
-          <div className="pae-axis pae-x-axis">
-            Scored residue
-          </div>
         </div>
-
-        <p>
-          AlphaFold produces a per-residue confidence score (pLDDT) between 0 and 100. Some regions with
-          low pLDDT may be unstructured in isolation.
-        </p>
-
-        <p>
-          The colour at position (x, y) indicates AlphaFold's expected position error at residue x, when the predicted and true structures are aligned on residue y.
-          <br/>
-          This is useful for assessing inter-domain accuracy.
-        </p>
-      </div>
-  );
+    );
 }
 
 export default AlphafoldInfoTable;
