46f901452af55d4ecc735c42b5b6742fd1bf34ac
prabhat <prabhat@ebi.ac.uk>
2024-05-12 22:34:46 +0100

    enforced react strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,18 @@
-import * as React from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import './styles/index.scss';
 import reportWebVitals from './reportWebVitals';
 
 import App from './ui/App';
 
-const container = document.getElementById('root');
-if (!container) throw new Error('Failed to find the root element');
-const root = createRoot(container);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-	<BrowserRouter basename={process.env.PUBLIC_URL}>
-		<App />
-	</BrowserRouter>
+	<React.StrictMode>
+		<BrowserRouter basename={process.env.PUBLIC_URL}>
+			<App/>
+		</BrowserRouter>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
