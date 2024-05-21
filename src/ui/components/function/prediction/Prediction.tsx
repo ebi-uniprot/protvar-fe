8c27ca1549114ba792f3a3d3b2767a1985cd66dd
prabhat <prabhat@ebi.ac.uk>
2024-05-21 13:22:07 +0100

    re-arranged predictions and added check for no preds

--- src/ui/components/function/prediction/Prediction.tsx
+++ src/ui/components/function/prediction/Prediction.tsx
@@ -6,15 +6,19 @@ import {EsmPred} from "./EsmPred";
 import {FoldxPred} from "./FoldxPred";
 import {Foldx} from "../../../../types/FunctionalResponse";
 import {useContext} from "react";
-import {StdColorContext} from "../../../App";
+import {AppContext} from "../../../App";
+import {CaddScorePred} from "./CaddScorePred";
+import {ColourCheckbox} from "../../../modal/ColourCheckbox";
 
 export type PredAttr = {
-  title: string,
+  text: string,
   color: string,
   stdColor: string,
-  info?: string
+  tip?: string,
+  range?: string
 }
 export const PUBMED_ID = {
+  CADD: 30371827,
   CONSERV: 11093265,
   AM: 37733863,
   EVE: 34707284,
@@ -24,14 +28,20 @@ export const PUBMED_ID = {
 }
 
 export const Prediction = (props: { record: MappingRecord, foldxs: Array<Foldx> }) => {
-  const stdColor = useContext(StdColorContext);
+  const {stdColor, toggleStdColor} = useContext(AppContext);
   return <><br/>
-    <ConservPred conserv={props.record.conservScore}/>
+    <ConservPred conserv={props.record.conservScore} stdColor={stdColor} />
     <b>Structure predictions</b><br/>
     <FoldxPred foldxs={props.foldxs}/>
     <b>Pathogenicity predictions</b><br/>
-    <AlphaMissensePred am={props.record.amScore} stdColor={stdColor} />
-    <EvePred eve={props.record.eveScore} stdColor={stdColor} />
-    <EsmPred esm={props.record.esmScore} stdColor={stdColor} />
+    {(!props.record.cadd && !props.record.amScore && !props.record.eveScore && !props.record.esmScore) &&
+      <div>No predictions available for this variant</div>
+    }
+    <CaddScorePred cadd={props.record.cadd} stdColor={stdColor}/>
+    <AlphaMissensePred am={props.record.amScore} stdColor={stdColor}/>
+    <EvePred eve={props.record.eveScore} stdColor={stdColor}/>
+    <EsmPred esm={props.record.esmScore} stdColor={stdColor}/>
+    <ColourCheckbox stdColor={stdColor} toggleStdColor={toggleStdColor}/>
   </>
 }
+
