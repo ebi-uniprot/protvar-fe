e8461c0a69d78386d350376b438a2ed2c90b99f3
prabhat <prabhat@ebi.ac.uk>
2024-06-08 12:36:30 +0100

    organised help sections

---
+++ public/api.md
@@ -0,0 +1,9 @@
+##### <a name="api"></a>API
+
+ProtVar REST API is a programmatic way to obtain information from ProtVar. You can query:
+
+- A list of variants via their genomic coordinates and choose which annotations you require. These can be posted as a list and then downloaded or emailed or a file can be uploaded.
+- Genomic coordinates to retrieve mappings to positions in proteins for all isoforms.
+- Individual amino acid positions to retrieve functional/structural/co-located variant annotations via a json object.
+
+REST API uses OpenAPI 3 which means you can use utils like openapi-generator to generate model classes.
--- src/ui/pages/help/HelpPage.tsx
+++ src/ui/pages/help/HelpPage.tsx
@@ -1,56 +1,93 @@
 import DefaultPageLayout from "../../layout/DefaultPageLayout";
 import "./HelpPage.css";
-import React, {useEffect, useState} from "react";
+import React, {Fragment, useEffect, useState} from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from 'remark-gfm';
 import rehypeRaw from "rehype-raw";
 import {uriTransformer} from "react-markdown";
-import {useLocation } from 'react-router-dom';
+import {useLocation} from 'react-router-dom';
 import {TITLE} from "../../../constants/const";
+import {ReactComponent as ChevronDownIcon} from "../../../images/chevron-down.svg"
+
+interface HelpSection {
+  title: string
+  content: string
+}
+
+let sections: HelpSection[] = [
+  {title: 'Website', content: ''},
+  {title: 'Download File', content: ''},
+  {title: 'API', content: ''},
+  {title: 'Direct Variant Link', content: ''}
+]
+
+const markdowns = ['website.md', 'download.md', 'api.md', 'directlink.md'];
+const fetchPromises = markdowns.map(url => fetch(url).then(response => response.text()));
+
+
+function loadContent() {
+  Promise.all(fetchPromises)
+    .then(responses => {
+      responses.map((response, index) => sections[index]["content"] = response);
+    })
+}
 
 function HelpPageContent() {
-    const [markdown, setMarkdown] = useState("")
-    useEffect(() => {
-        document.title = 'Help - ' + TITLE;
-    }, []);
+  const [expandedRowKey, setExpandedRowKey] = useState('')
+  function toggleRow(key: string) {
+    setExpandedRowKey(expandedRowKey === key ? '' : key)
+  }
 
-    const addHelpPrefix = function (uri: string) {
-        if (uri.startsWith('#'))
-            return window.location.pathname + uri
-        return uriTransformer(uri)
-    }
+  useEffect(() => {
+    document.title = 'Help - ' + TITLE;
+    loadContent();
+  }, []);
 
-    useEffect(() => {
-        fetch(window.location.pathname + ".md")
-            .then((res) => res.text())
-            .then((text) => setMarkdown(text));
-    }, []);
+  const addHelpPrefix = function (uri: string) {
+    if (uri.startsWith('#'))
+      return window.location.pathname + uri
+    return uriTransformer(uri)
+  }
 
-
-    const { hash } = useLocation();
-    useEffect(() => {
-        if (!markdown)
-            return
-        // if not a hash link, scroll to top
-        if (hash === '') {
-            window.scrollTo(0, 0);
-        } else { // else scroll to id
-            setTimeout(() => {
-                const id = hash.replace('#', '');
-                const element = document.getElementById(id);
-                if (element) {
-                    element.scrollIntoView();
-                }
-            }, 0);
+  const {hash} = useLocation();
+  useEffect(() => {
+    // if not a hash link, scroll to top
+    if (hash === '') {
+      window.scrollTo(0, 0);
+    } else { // else scroll to id
+      setTimeout(() => {
+        const id = hash.replace('#', '');
+        const element = document.getElementById(id);
+        if (element) {
+          element.scrollIntoView();
         }
-    }, [markdown, hash]); // do this on route change
+      }, 0);
+    }
+  }, [hash]); // do this on route change
 
-    return <div className="container help">
-        <ReactMarkdown rehypePlugins={[rehypeRaw]} transformLinkUri={addHelpPrefix} remarkPlugins={[remarkGfm]}>{markdown}</ReactMarkdown>
-    </div>
+  return <div className="container help">
+    <h4>Help</h4>
+    {
+      Object.values(sections).map((s: HelpSection, index: number) => {
+        let key = `help#sec-${index + 1}`
+        return <Fragment key={key}>
+          <button type="button" className="collapsible" onClick={(e) => toggleRow(key)}>
+              {`${index+1}. ${s.title}`}
+            <ChevronDownIcon className="chevronicon"/>
+          </button>
+          {(key === expandedRowKey) && <div style={{padding: '20px'}}>
+            <ReactMarkdown rehypePlugins={[rehypeRaw]} transformLinkUri={addHelpPrefix}
+                           remarkPlugins={[remarkGfm]}>{s.content}</ReactMarkdown>
+          </div>
+          }
+        </Fragment>
+      })
+    }
+  </div>
 }
 
 function HelpPage() {
-    return <DefaultPageLayout content={<HelpPageContent />} />
+  return <DefaultPageLayout content={<HelpPageContent/>}/>
 }
+
 export default HelpPage;
