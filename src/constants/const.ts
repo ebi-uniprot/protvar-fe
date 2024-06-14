e1cc5c04414919ed6777e94e6810087664f5f16c
prabhat <prabhat@ebi.ac.uk>
2024-06-14 16:24:53 +0100

    constant clean up

--- src/constants/const.ts
+++ src/constants/const.ts
@@ -2,14 +2,16 @@ export const API_URL = process.env.REACT_APP_API_BASE_URL;
 export const NO_OF_ITEMS_PER_PAGE = 25;
 export const PAGE_SIZES = [25, 50, 100]
 export const PAGE_SIZE = 25
-export const MAX_IN_PLACE_DOWNLOAD_WITHOUT_EMAIL = 25;
 export const G2P_MAPPING_URI = `${API_URL}/mappings`;
-export const DOWNLOAD_URI = `${API_URL}/download/stream`;
-export const EMAIL_URI = `${API_URL}/email/process`;
-export const API_HEADERS = { "Content-Type": "application/json", Accept: "*" };
+export const DEFAULT_HEADERS = {"Content-Type": "application/json", Accept: "*"};
+
+export const CONTENT_TEXT = {"Content-Type": "text/plain"}
+export const CONTENT_MULTIPART = {"Content-Type": "multipart/form-data"}
 export const DOWNLOAD_STATUS=`${API_URL}/download/status`;
 export const LOCAL_DOWNLOADS='PV_downloads';
 export const DISMISS_BANNER = 'PV_banner';
+
+// TODO resubscribe option - clears the localData
 export const SUBSCRIPTION_STATUS = 'PV_subscribed';
 export const TITLE='EMBL-EBI ProtVar - Contextualising human missense variation'
 export const PV_FTP = 'https://ftp.ebi.ac.uk/pub/databases/ProtVar'
