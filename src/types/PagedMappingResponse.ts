887da2e1f520451c15d10b9cdf3e969e6a546c82
prabhat <prabhat@ebi.ac.uk>
2024-06-17 16:31:18 +0100

    create a paged response from a single mapping response

--- src/types/PagedMappingResponse.ts
+++ src/types/PagedMappingResponse.ts
@@ -1,4 +1,5 @@
 import { MappingResponse } from "./MappingResponse";
+import {PAGE_SIZE} from "../constants/const";
 
 export enum ResultType {SEARCH, PROTEIN}
 export interface PagedMappingResponse {
@@ -9,6 +10,17 @@ export interface PagedMappingResponse {
   totalItems: number
   totalPages: number
   last: boolean
+}
+
+export const toPagedMappingResponse = (mappingResponse: MappingResponse): PagedMappingResponse => {
+  return {content: mappingResponse,
+    id: "",
+    page: 1,
+    pageSize: PAGE_SIZE,
+    totalItems: 1,
+    totalPages: 1,
+    last: true
+  }
 }
 
 export interface IDResponse {
