2d4470e1729f7eafe7967c27416006a4917655cb
prabhat <prabhat@ebi.ac.uk>
2024-10-01 15:39:34 +0100

    added icon to sidebar menu option, renamed Results to Search History

--- src/ui/layout/DefaultPageContent.tsx
+++ src/ui/layout/DefaultPageContent.tsx
@@ -5,6 +5,7 @@ import RecentResult from "../pages/result/RecentResult";
 import {LOCAL_DOWNLOADS, LOCAL_RESULTS} from "../../constants/const";
 import useLocalStorage from "../../hooks/useLocalStorage";
 import {SET_ITEM} from "../../context/LocalStorageContext";
+import Spaces from "../elements/Spaces";
 
 const DefaultPageContent = (props: {
   children: JSX.Element
@@ -37,14 +38,14 @@ const DefaultPageContent = (props: {
         <nav>
           <ul>
             <li className="sidebar-menu">
-              <NavLink to={HOME}>Search</NavLink>
+              <NavLink to={HOME}><i className="bi bi-search"></i><Spaces count={2} />Search</NavLink>
             </li>
             <li className="sidebar-menu">
-              <NavLink to={RESULT}>Results <div className={`record-count ${numResults === 0 ? `gray-bg` : ``}`}>{numResults}</div></NavLink>
+              <NavLink to={RESULT}><i className="bi bi-files"></i><Spaces count={2}/>Search History <div className={`record-count ${numResults === 0 ? `gray-bg` : ``}`}>{numResults}</div></NavLink>
               <RecentResult/>
             </li>
             <li className="sidebar-menu">
-              <NavLink to={DOWNLOAD}>Downloads <div className={`record-count ${numDownloads === 0 ? `gray-bg` : ``}`}>{numDownloads}</div></NavLink>
+              <NavLink to={DOWNLOAD}><i className="bi bi-download"></i><Spaces count={2}/>Downloads <div className={`record-count ${numDownloads === 0 ? `gray-bg` : ``}`}>{numDownloads}</div></NavLink>
             </li>
           </ul>
         </nav>
