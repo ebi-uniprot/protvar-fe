2161f15ef8a6ad5fe87c0a99fc1b5f8fbda04eef
prabhat <prabhat@ebi.ac.uk>
2025-04-11 10:30:56 +0100

    css changes

--- src/ui/modal/Modal.jsx
+++ src/ui/modal/Modal.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Modal = ({ handleClose, show, children }) => {
-	const showHideClassName = show ? 'modal d-block' : 'modal d-none';
+	const showHideClassName = show ? 'modal2 d-block' : 'modal2 d-none';
 
 	return (
 		<div className={showHideClassName}>
-			<div className="modal-container">{children}</div>
+			<div className="modal-container2">{children}</div>
 		</div>
 	);
 };
